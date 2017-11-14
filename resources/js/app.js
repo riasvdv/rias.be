ec8115917f4ee68e3f13a24152f0081376c2503f
Rias <hello@rias.be>
2017-11-14 22:34:21 +0100

    Optimize font loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,3 +8,18 @@ require('./bootstrap');
 $(function() {
     $('a[rel="fluidbox"]').fluidbox();
 });
+
+const FontFaceObserver = require('fontfaceobserver');
+const Cookies = require('js-cookie');
+
+// if the class is already set, we're good.
+if( document.documentElement.className.indexOf( "fonts-loaded" ) === -1 ){
+    let fontA = new FontFaceObserver( "Fira Mono", { weight: 400 });
+    let fontB = new FontFaceObserver( "Fira Mono", { weight: 700 });
+    Promise
+        .all([fontA.load(), fontB.load()])
+        .then(function(){
+            document.documentElement.className += " fonts-loaded";
+            Cookies.set('fonts-loaded', true);
+        });
+}
