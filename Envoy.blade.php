e086fb1a2f7aeb8279d79ea3503943995246a9a5
Rias <hello@rias.be>
2019-04-23 20:28:11 +0200

    wip

---
+++ Envoy.blade.php
@@ -0,0 +1,147 @@
+@setup
+$server = "134.209.193.221";
+$userAndServer = 'forge@'. $server;
+$repository = "rias500/rias.be";
+$baseDir = "/home/forge/rias.be";
+$releasesDir = "{$baseDir}/releases";
+$persistentDir = "{$baseDir}/persistent";
+$currentDir = "{$baseDir}/current";
+$newReleaseName = date('Ymd-His');
+$newReleaseDir = "{$releasesDir}/{$newReleaseName}";
+$user = get_current_user();
+
+function logMessage($message) {
+return "echo '\033[32m" .$message. "\033[0m';\n";
+}
+@endsetup
+
+@servers(['local' => '127.0.0.1', 'remote' => $userAndServer])
+
+@macro('deploy')
+startDeployment
+cloneRepository
+runComposer
+runYarn
+generateAssets
+updateSymlinks
+migrateDatabase
+blessNewRelease
+cleanOldReleases
+finishDeploy
+@endmacro
+
+@macro('deploy-code')
+deployOnlyCode
+@endmacro
+
+@task('startDeployment', ['on' => 'local'])
+{{ logMessage("🏃  Starting deployment…") }}
+git checkout master
+git pull origin master
+@endtask
+
+@task('cloneRepository', ['on' => 'remote'])
+{{ logMessage("🌀  Cloning repository…") }}
+[ -d {{ $releasesDir }} ] || mkdir {{ $releasesDir }};
+[ -d {{ $persistentDir }} ] || mkdir {{ $persistentDir }};
+[ -d {{ $persistentDir }}/storage ] || mkdir {{ $persistentDir }}/storage;
+cd {{ $releasesDir }};
+
+# Create the release dir
+mkdir {{ $newReleaseDir }};
+
+# Clone the repo
+git clone --depth 1 git@github.com:{{ $repository }} {{ $newReleaseName }}
+
+# Configure sparse checkout
+cd {{ $newReleaseDir }}
+git config core.sparsecheckout true
+echo "*" > .git/info/sparse-checkout
+echo "!storage" >> .git/info/sparse-checkout
+echo "!web/build" >> .git/info/sparse-checkout
+git read-tree -mu HEAD
+
+# Mark release
+cd {{ $newReleaseDir }}
+echo "{{ $newReleaseName }}" > web/release-name.txt
+@endtask
+
+@task('runComposer', ['on' => 'remote'])
+cd {{ $newReleaseDir }};
+{{ logMessage("🚚  Running Composer…") }}
+ln -nfs {{ $baseDir }}/auth.json auth.json;
+composer install --prefer-dist --no-scripts --no-dev -q -o;
+@endtask
+
+@task('runYarn', ['on' => 'remote'])
+{{ logMessage("📦  Running Yarn…") }}
+cd {{ $newReleaseDir }};
+yarn config set ignore-engines true
+yarn
+@endtask
+
+@task('generateAssets', ['on' => 'remote'])
+{{ logMessage("🌅  Generating assets…") }}
+cd {{ $newReleaseDir }};
+yarn run production --progress false
+@endtask
+
+@task('updateSymlinks', ['on' => 'remote'])
+{{ logMessage("🔗  Updating symlinks to persistent data…") }}
+# Remove the storage directory and replace with persistent data
+rm -rf {{ $newReleaseDir }}/storage;
+cd {{ $newReleaseDir }};
+ln -nfs {{ $baseDir }}/persistent/storage storage;
+
+# Remove the web/assets directory and replace with persistent data
+rm -rf {{ $newReleaseDir }}/web/assets;
+cd {{ $newReleaseDir }};
+ln -nfs {{ $baseDir }}/persistent/storage/assets web/assets;
+
+# Import the environment config
+cd {{ $newReleaseDir }};
+ln -nfs {{ $baseDir }}/.env .env;
+@endtask
+
+@task('migrateDatabase', ['on' => 'remote'])
+{{ logMessage("🙈  Migrating database…") }}
+cd {{ $newReleaseDir }};
+php craft migrate/all;
+@endtask
+
+@task('syncProjectConfig', ['on' => 'remote'])
+{{ logMessage("🙈  Syncing project config…") }}
+cd {{ $newReleaseDir }};
+php craft migrate/all;
+php craft project-config/sync
+@endtask
+
+@task('blessNewRelease', ['on' => 'remote'])
+{{ logMessage("🙏  Blessing new release…") }}
+ln -nfs {{ $newReleaseDir }} {{ $currentDir }};
+cd {{ $newReleaseDir }}
+#php craft clear-caches/all;
+
+sudo -S /usr/sbin/service php7.3-fpm reload
+@endtask
+
+@task('cleanOldReleases', ['on' => 'remote'])
+{{ logMessage("🚾  Cleaning up old releases…") }}
+# Delete all but the 2 most recent.
+cd {{ $releasesDir }}
+ls -dt {{ $releasesDir }}/* | tail -n +3 | xargs -d "\n" sudo chown -R forge .;
+ls -dt {{ $releasesDir }}/* | tail -n +3 | xargs -d "\n" rm -rf;
+@endtask
+
+@task('finishDeploy', ['on' => 'local'])
+{{ logMessage("🚀  Application deployed!") }}
+@endtask
+
+@task('deployOnlyCode',['on' => 'remote'])
+{{ logMessage("💻  Deploying code changes…") }}
+cd {{ $currentDir }}
+git pull origin master
+#php craft clear-caches/all
+#php craft project-config/sync
+sudo -S /usr/sbin/service php7.3-fpm reload
+@endtask
