f670a88a0f0d262b14b3b8068575758101e4548f
Rias <hello@rias.be>
2019-08-20 09:32:28 +0200

    Don't run locally

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -19,7 +19,7 @@ return "echo '\033[32m" .$message. "\033[0m';\n";
 @servers(['local' => '127.0.0.1', 'remote' => $userAndServer])
 
 @macro('deploy')
-startDeployment
+#startDeployment
 cloneRepository
 runComposer
 runYarn
