28077ce0717b3abb5367b9696572057f2a3acc83
Rias <hello@rias.be>
2019-12-19 10:45:55 +0100

    Add offline page + serviceworker caching

---
+++ content/collections/pages/offline.md
@@ -0,0 +1,16 @@
+---
+title: 'Your connection seems down'
+contents:
+  -
+    type: set
+    attrs:
+      values:
+        type: image
+        image:
+          - offline.gif
+  -
+    type: paragraph
+updated_by: ada9a6ce-9291-49fe-b0fb-1ba6bb9655cf
+updated_at: 1576748462
+id: 3082a70a-032e-4323-a5e2-50f3142709fc
+---
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,9 @@ const mix = require('laravel-mix');
 
 require('laravel-mix-purgecss');
 
-mix.js('resources/js/site.js', 'public/js')
+mix
+    .js('resources/js/site.js', 'public/js')
+    .js('resources/js/sw.js', 'public')
 
     .postCss('resources/css/site.css', 'public/css', [require('tailwindcss')('tailwind.config.js')])
 
