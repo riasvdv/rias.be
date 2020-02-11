43a6a31e91770f50f23e3d72489514f7b8466185
Rias <hello@rias.be>
2020-02-11 11:57:32 +0100

    Add new purgeCss

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,18 +6,16 @@ mix
     .js('resources/js/site.js', 'public/js')
     .js('resources/js/sw.js', 'public')
 
-    .postCss('resources/css/site.css', 'public/css', [require('tailwindcss')('tailwind.config.js')])
+    .postCss('resources/css/site.css', 'public/css', [
+        require('tailwindcss')('tailwind.config.js')
+    ])
+
+    .purgeCss({
+        whitelistPatterns: [/pre/, /hljs/, /highlighted/, /text-teal/, /text-orange/],
+    })
 
     .options({
         processCssUrls: false,
     })
 
-    .version()
-
-    .purgeCss({
-        enabled: mix.inProduction(),
-        globs: [
-            path.join(__dirname, 'resources/views/**/*.{html,php}'),
-        ],
-        whitelistPatterns: [/pre/, /hljs/, /highlighted/, /text-teal/, /text-orange/],
-    });
+    .version();
