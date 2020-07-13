33f4f4e17b351ba6224d31c7b6807aef56b5f0c4
Rias <hello@rias.be>
2020-07-13 11:55:01 +0200

    wip

--- build.sh
+++ build.sh
@@ -5,7 +5,15 @@ yum install -y amazon-linux-extras
 amazon-linux-extras enable php7.4
 yum clean metadata
 yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
-yum install wget libwebp
+yum install wget
+
+# Install Webp support
+wget https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-0.5.0.tar.gz
+tar xvzf libwebp-0.5.0.tar.gz
+cd libwebp-0.5.0
+sudo ./configure
+sudo make
+sudo make install
 
 # INSTALL COMPOSER
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
