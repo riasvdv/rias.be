78226f7005602b28bd9805e1a07e6c9c96574506
Rias <hello@rias.be>
2020-07-14 09:33:48 +0200

    Woops

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,12 @@
 
 namespace App\Providers;
 
-use Facade\Ignition\Facades\Flare;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Flare::group('Statamic', [
-            'view' => "<pre>" . view('errors.404')->toHtml() . "</pre>",
-        ]);
     }
 
     public function register()
