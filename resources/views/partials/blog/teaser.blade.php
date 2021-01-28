5271e2a07e4179298811eec5fb81ebedad85f68b
Rias <hey@rias.be>
2021-01-28 18:08:14 +0100

    purge

--- resources/views/partials/blog/teaser.blade.php
+++ resources/views/partials/blog/teaser.blade.php
@@ -1,4 +1,4 @@
-<!-- border-red-500 border-orange-500 border-yellow-500 border-green-500 border-teal-500 border-blue-500 border-indigo-500 border-purple-500 border-pink-500 -->
+<!-- bg-red-500 bg-orange-500 bg-yellow-500 bg-green-500 bg-teal-500 bg-blue-500 bg-indigo-500 bg-purple-500 bg-pink-500 -->
 <div class="group bg-gray-100 transition-all leading-normal p-8 rounded-md transform hover:-translate-y-2 hover:shadow-md">
     <a href="{{ $entry->augmentedValue('url') }}" class="flex flex-col h-full no-shadow block text-gray-700 no-underline mb-4 hover:text-gray-900">
         <span class="flex gap-2 mb-8 items-center">
