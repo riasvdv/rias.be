816d3b495f7f61d0ecd2d022000dde7602f9fd7c
Rias <hey@rias.be>
2021-01-28 19:26:54 +0100

    mobile blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container mx-auto">
-    <div class="grid grid-cols-3 gap-8">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         @foreach (tag('collection:blog') as $entry)
             @include('partials.blog.teaser')
         @endforeach
