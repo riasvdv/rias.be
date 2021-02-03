4840a1a3b2e270dede0bcb77206ffb077349d1e5
Rias <hey@rias.be>
2021-02-03 16:04:16 +0100

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         @if (tag('collection:count', ['in' => 'blog']) > 0)
             <section class="my-24">
                 <div class="text-center my-8">
-                    <h3 class="text-base text-2xl text-gray-400 tracking-widest uppercase mb-4">Blog</h3>
+                    <h3 class="text-base text-2xl text-gray-500 tracking-widest uppercase mb-4">Blog</h3>
                 </div>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
                     @foreach (tag('collection:blog', ['limit' => 3]) as $entry)
@@ -26,7 +26,7 @@
         @if (tag('collection:count', ['in' => 'addons']) > 0)
             <section class="my-24">
                 <div class="text-center my-8">
-                    <h3 class="text-base text-2xl text-gray-400 tracking-widest uppercase mb-4">Addons / Packages</h3>
+                    <h3 class="text-base text-2xl text-gray-500 tracking-widest uppercase mb-4">Addons / Packages</h3>
                 </div>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
                     @foreach (tag('collection:addons') as $entry)
