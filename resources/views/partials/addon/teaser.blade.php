17c450437ba4529ae4e0bd9fa237a78c5a8ac49f
Rias <hey@rias.be>
2021-02-03 16:00:47 +0100

    Add noopener to external link

--- resources/views/partials/addon/teaser.blade.php
+++ resources/views/partials/addon/teaser.blade.php
@@ -1,6 +1,6 @@
 <!-- bg-red-500 bg-orange-500 bg-yellow-500 bg-green-500 bg-teal-500 bg-blue-500 bg-indigo-500 bg-purple-500 bg-pink-500 -->
 <div class="bg-gray-100 transition-all leading-normal p-8 rounded-md transform hover:-translate-y-2 hover:shadow-md">
-    <a href="{{ $entry->augmentedValue('link') }}" target="_blank" class="flex flex-col h-full no-shadow block text-gray-700 no-underline mb-4 hover:text-gray-900">
+    <a href="{{ $entry->augmentedValue('link') }}" target="_blank" rel="noopener" class="flex flex-col h-full no-shadow block text-gray-700 no-underline mb-4 hover:text-gray-900">
         <span class="flex gap-2 mb-8 items-center">
             <i class="inline-block rounded-full bg-{{ $entry->value('color') ? $entry->value('color')['label'] : '' }}-500 w-4 h-4"></i>
             <h3 class="text-lg font-bold mt-px">{{ $entry->augmentedValue('title') }}</h3>
