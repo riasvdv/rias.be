2054afaf7cb46ed94fee85a2a66ab2cd6519ed65
Rias <hey@rias.be>
2021-02-05 22:48:37 +0100

    Need pdo

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 yum install -y amazon-linux-extras
 amazon-linux-extras enable php7.4
 yum clean metadata
-yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
+yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap,pdo,pdo_mysql}
 yum install wget
 
 # INSTALL COMPOSER
