d1b232e25bcf7e7fa79f5d0530e1eea1ee1e712b
Rias <hey@rias.be>
2021-02-11 13:12:37 +0100

    Move feed to blade

---
+++ resources/views/layouts/feed.blade.php
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <id>https://www.rias.be/feed</id>
+    <link href="https://www.rias.be/feed" rel="self"></link>
+    <title><![CDATA[Rias.be]]></title>
+    @foreach (tag('collection:blog', ['limit' => 1]) as $entry)
+    <updated>{{ $entry->date()->format('c') }}</updated>
+    @endforeach
+    @foreach (tag('collection:blog') as $entry)
+    <entry>
+        <title><![CDATA[{{ $entry->value('title') }}]]></title>
+        <link rel="alternate" href="https://www.rias.be{{ $entry->url() }}" />
+        <id>https://www.rias.be{{ $entry->url() }}</id>
+        <author>
+            <name> <![CDATA[Rias Van der Veken]]></name>
+        </author>
+        <?php $header = collect($entry->augmentedValue('contents')->value())->firstWhere('type', 'header') ?>
+        @if ($header)
+            <summary type="html">
+                <![CDATA[{!! $header['header'] !!}]]>
+            </summary>
+        @endif
+        <updated>{{ $entry->date()->format('c') }}</updated>
+    </entry>
+    @endforeach
+</feed>
