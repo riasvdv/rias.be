819d4edaf324fdda04985cb8aed91a7bceaf58d7
Rias <hey@rias.be>
2021-03-19 16:25:47 +0100

    wip

--- resources/views/partials/sets/header.blade.php
+++ resources/views/partials/sets/header.blade.php
@@ -1,8 +1,10 @@
 <header class="mb-4 flex items-center justify-between">
     <div class="prose prose {{ $url === '/' ? 'prose-xl md:prose-2xl max-w-3xl' : 'prose-xl mx-auto'}}">{!! $header !!}</div>
-    <div class="hidden lg:block flex-shrink-0 mx-auto">
-        @responsive($image, [
-            'class' => 'rounded-full h-72 w-72',
-        ])
-    </div>
+    @isset($image)
+        <div class="hidden lg:block flex-shrink-0 mx-auto">
+            @responsive($image, [
+                'class' => 'rounded-full h-72 w-72',
+            ])
+        </div>
+    @endisset
 </header>
