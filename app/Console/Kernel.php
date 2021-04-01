4cfd50b32cc5c2342d6881255d2ae5f4e44aa6bd
Rias <hey@rias.be>
2021-04-01 10:30:05 +0200

    Add sync

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SyncPaymentsToAccountableCommand;
+use App\Console\Commands\SyncStripePaymentsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command(SyncStripePaymentsCommand::class)->hourly();
+        $schedule->command(SyncPaymentsToAccountableCommand::class)->hourly();
     }
 
     /**
