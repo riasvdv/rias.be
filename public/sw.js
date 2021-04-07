fdd819f41dbaa85ff244967f299b9284ed314e16
Rias <hey@rias.be>
2021-04-07 09:16:25 +0200

    Add temporary sw

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("activate", function(e) {
+    self.registration.unregister()
+        .then(function() {
+            return self.clients.matchAll();
+        })
+        .then(function(clients) {
+            clients.forEach(client => client.navigate(client.url));
+        });
+});
