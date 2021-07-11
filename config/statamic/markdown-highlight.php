e3bc4ec91e32b15062b846420608cf064dc170e6
Rias <hey@rias.be>
2021-07-11 12:29:31 +0200

    theme

--- config/statamic/markdown-highlight.php
+++ config/statamic/markdown-highlight.php
@@ -7,7 +7,7 @@ return [
      * themes work with Shiki https://github.com/shikijs/shiki/blob/master/docs/themes.md#all-themes
      * you can also pass in an absolute path to a custom VS Code theme.
      */
-    'theme' => 'github-light',
+    'theme' => __DIR__ . '/../ayu-light.json',
 
     /**
      * Whether the result should be cached, the cache key and invalidation
