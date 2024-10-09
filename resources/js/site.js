9d3a0e64d2465227223ca2a74093585f2c0d37c5
Rias <hey@rias.be>
2024-10-09 15:47:11 +0200

    Revert "Try posthog"
    This reverts commit c267031086a4f76c1a23fb3966951e1a4a64223d.

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,4 +1,3 @@
-import posthog from 'posthog-js'
 import '../css/site.css';
 import "./webmentions";
 
@@ -7,10 +6,3 @@ document.addEventListener('DOMContentLoaded', (event) => {
         document.querySelector('#mobile-nav').classList.add('loaded');
     }, 250);
 });
-
-posthog.init('phc_Pj8t6puD56t0XmSTmiABmXEFhADd3eN1AFQf3aUaGsP',
-    {
-        api_host: 'https://eu.i.posthog.com',
-        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-    }
-)
