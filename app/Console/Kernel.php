af0220838ff61963938571a4e1ed3890e611865f
riasvdv <riasvdv@users.noreply.github.com>
2025-02-04 12:48:05 +0000

    Fix styling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,9 +7,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected function schedule(Schedule $schedule)
-    {
-    }
+    protected function schedule(Schedule $schedule) {}
 
     protected function commands()
     {
