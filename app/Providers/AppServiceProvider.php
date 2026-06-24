6ae7d8d0859f0cfd2b584300e595aca78a63e944
Rias <hey@rias.be>
2026-06-24 09:12:56 +0200

    Switch to Markdown field

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Markdown::extend('original', new PhikiCommonMarkFlavor);
+        Markdown::extend('gfm', new PhikiCommonMarkFlavor);
     }
 }
